f650f3e52c96986604c11f69d72f2b3c3c81d503
Filipe Firmino <filipefirmino@gec.inatel.br>
2024-10-25 10:37:00 -0300

    :feat: ajustando para o build

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,13 +2,14 @@ import { MongoConnectionOptions } from 'typeorm/driver/mongodb/MongoConnectionOp
 import * as dotenv from 'dotenv';
 import {UserEntity} from "../user/domain/entity/user.entity";
 import {MinistrieEntity} from "../ministrie/domain/entity/ministrie.entity";
+import * as process from "process";
 
 dotenv.config();
 
 export const ORMConfig: MongoConnectionOptions = {
   type: 'mongodb',
   entities: [UserEntity, MinistrieEntity],
-  url: 'mongodb+srv://filipefirmino:P22J9JoyBsyMrc6M@ibbclustermongodb.meo2m.mongodb.net/?retryWrites=true&w=majority&appName=IbbClusterMongoDb',
+  url: process.env.DB_URL,
   useUnifiedTopology: true,
   logging: true,
   synchronize: true,
